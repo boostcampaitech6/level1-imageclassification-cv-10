eff44fa78b4201dd8a32a1ddf435c3f63351b570
JasonPark <cjpark137@naver.com>
2023-12-13 18:17:10 +0900

    feat: addition hpo on argparser

--- utils/argparsers.py
+++ utils/argparsers.py
@@ -116,3 +116,9 @@ class Parser(object):
             help="The model"
         )
         
+        # HPO
+        self.parser.add_argument(
+            '--hpo',
+            default=True,
+            help="Try HPO"
+        )
