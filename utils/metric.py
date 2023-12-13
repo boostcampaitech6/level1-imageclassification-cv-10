6bd10e339641b6bf9253c24b82114aadf57ffc78
jini <qkqh9912@gmail.com>
2023-12-13 16:32:38 +0900

    # feat tqdm, confusion matrix, metric by classes

---
+++ utils/metric.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def calculate_metrics(true_labels: list[int], predicted_labels: list[int], num_classes: int) -> dict:
+    tp = [0] * num_classes
+    fp = [0] * num_classes
+    fn = [0] * num_classes
+
+    accuracy = 0
+    precision = [0] * num_classes
+    recall = [0] * num_classes
+    f1_score = [0] * num_classes
+
+    for i in range(len(true_labels)):
+        if true_labels[i] == predicted_labels[i]:
+            tp[true_labels[i]] += 1
+        else:
+            fp[predicted_labels[i]] += 1
+            fn[true_labels[i]] += 1
+
+    accuracy = sum(tp) / len(true_labels)
+
+    for i in range(num_classes):
+        precision[i] = tp[i] / (tp[i] + fp[i]) if (tp[i] + fp[i]) > 0 else 0
+        recall[i] = tp[i] / (tp[i] + fn[i]) if (tp[i] + fn[i]) > 0 else 0
+        f1_score[i] = 2 * (precision[i] * recall[i]) / (precision[i] + recall[i]) if (precision[i] + recall[i]) > 0 else 0
+
+    return {"Total Accuracy": accuracy, "Precision": precision, "Recall": recall, "F1 Score": f1_score, 
+            "Total Precision": sum(precision) / num_classes, "Total Recall": sum(recall) / num_classes,
+            "Total F1 Score": sum(f1_score) / num_classes}
+    
+def parse_metric(metrics: dict, label_name: list[str]=None) -> str:
+    class_num = len(metrics["F1 Score"])
+    if label_name is None:
+        label_name = []
+        for idx in range(class_num):
+            label_name.append(str(idx))
+    
+    parsed_string = ""
+    zipped_metrics = zip(metrics["Precision"], metrics["Recall"], metrics["F1 Score"])
+    parse_format = "{:>25} Precision {:3.4f}, Recall {:3.4f}, F1 Score {:3.4f}"
+    for idx, (precision, recall, f1_score) in enumerate(zipped_metrics):
+        parsed_string += parse_format.format(label_name[idx], precision, recall, f1_score) + "\n"
+    
+    parse_format += " Acc. {:3.4f}"
+    parsed_string += parse_format.format("Total", metrics["Total Precision"], \
+        metrics["Total Recall"], metrics["Total F1 Score"], metrics["Total Accuracy"]) + "\n"
+    
+    return parsed_string
+    
+
+    
+        
---
+++ utils/plot.py
@@ -0,0 +1,45 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from data.dataset import MaskBaseDataset
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import os
+
+def grid_image(np_images, gts, preds, n=16, shuffle=False):
+    batch_size = np_images.shape[0]
+    assert n <= batch_size
+
+    choices = random.choices(range(batch_size), k=n) if shuffle else list(range(n))
+    figure = plt.figure(figsize=(12, 18 + 2)) 
+    plt.subplots_adjust(top=0.8)              
+    n_grid = np.ceil(n ** 0.5)
+    tasks = ["mask", "gender", "age"]
+    for idx, choice in enumerate(choices):
+        gt = gts[choice].item()
+        pred = preds[choice].item()
+        image = np_images[choice]
+        # title = f"gt: {gt}, pred: {pred}"
+        gt_decoded_labels = MaskBaseDataset.decode_multi_class(gt)
+        pred_decoded_labels = MaskBaseDataset.decode_multi_class(pred)
+        title = "\n".join([
+            f"{task} - gt: {gt_label}, pred: {pred_label}"
+            for gt_label, pred_label, task
+            in zip(gt_decoded_labels, pred_decoded_labels, tasks)
+        ])
+
+        plt.subplot(int(n_grid), int(n_grid), int(idx + 1), title=title)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(image, cmap=plt.cm.binary)
+
+    return figure
+
+def save_confusion_matrix(target: np.ndarray, prediction: np.ndarray, label_num: int, save_path: str):
+    matrix = confusion_matrix(target, prediction)
+    plt.figure(figsize=(12, 9)) 
+    sns.heatmap(matrix, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted')
+    plt.ylabel('Ground Truth')
+    plt.savefig(os.path.join(save_path, 'confusion_matrix.jpg'))
