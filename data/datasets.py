1803b1087c353c0f1ea5b05862e736940be65a9a
lukehanjun <70832671+lukehanjun@users.noreply.github.com>
2023-12-18 10:58:36 +0900

    feat: seperate train and val set & add 4 new augmentations (#42)
    * feat: seperate train and val set
    
    * feat: add 4 new augmentations
    
    * feat: add cutmix base code

--- data/datasets.py
+++ data/datasets.py
@@ -97,16 +97,15 @@ class MaskBaseDataset(Dataset):
         "normal": MaskLabels.NORMAL
     }
 
-    image_paths = []
-    mask_labels = []
-    gender_labels = []
-    age_labels = []
-
     def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
         self.data_dir = data_dir
         self.mean = mean
         self.std = std
         self.val_ratio = val_ratio
+        self.image_paths = []
+        self.mask_labels = []
+        self.gender_labels = []
+        self.age_labels = []
 
         self.transform = None
         self.setup()
