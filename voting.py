75004e61ec0f6cee024c220655bef1e863e1acc4
JasonPark <cjpark137@naver.com>
2023-12-20 00:33:03 +0900

    feat: add voting

---
+++ voting.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+#submit csv path를 지정!!
+submit_csv = '/data/ephemeral/home/input/eval/info.csv' #ex) '../data/submit.csv'
+
+#Ensemble할 model이 모여있는 경로를 지정 (ex - 3개의 모델이 한 폴더에 위치)
+path = '/data/ephemeral/home/level1-imageclassification-cv-10/output' #ex) './Ensemble/'
+csv_list = [os.path.join(path, f) for  f in os.listdir(path)]
+print(csv_list)
+submit = pd.read_csv(f'{submit_csv}', index_col=False)
+
+temp = pd.DataFrame()
+for index, csv in enumerate(csv_list):
+    temp[f'{index}'] = pd.read_csv(csv, index_col=False)['ans']
+
+submit['ans'] = temp.mode(axis=1)[0].astype('int')
+print(submit)
+
+submit.to_csv(f'{path}voting.csv', index=False)
+print('Done!!')
