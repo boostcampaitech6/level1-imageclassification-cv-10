03f58ff47aee0fdb3249b9062e6429b20557890e
sihyun <classaen7@naver.com>
2023-12-20 02:26:45 +0000

    feat: combine_folder

--- utils/combine_image_folder.py
+++ utils/combine_image_folder.py
@@ -1,19 +1,28 @@
 import os
 import shutil
 
+"""
+-> 소윤님이 작성하신 train_va_split.py로 분할한 뒤에 사용하시면 됩니다.
+
+dataset_train_dir = ../input/dataset/train : 원본 훈련 데이터 폴더에 있는 이미지들과
+rembg_dataset_train_dir = ../input/rembg_dataset/train : rembg로 변환된 훈련 데이터의 이미지들을 합치는 코드 파일입니다.
+
+
+all_dataset_train_dir은 이 합치는 파일이 저장되는 경로 입니다. -> 알아서 파일명 기입하시면 됩니다
+
+
+"""
+
 # 디렉토리 경로 설정
 input_dir = '../input'
 dataset_train_dir = os.path.join(input_dir, 'dataset', 'train')
-rembg_dataset_train_dir = os.path.join(input_dir, 'rembg_dataset_4', 'train')
-yolo_dataset_train_dir = os.path.join(input_dir, 'yolo_dataset_4', 'train')
-all_dataset_train_dir = os.path.join(input_dir, 'all_age_4_dataset', 'train')
+rembg_dataset_train_dir = os.path.join(input_dir, 'rembg_dataset', 'train')
+all_dataset_train_dir = os.path.join(input_dir, 'all_dataset', 'train')
 
-# all_dataset/train 디렉토리가 없으면 생성
+# 새로 저장할 디렉토리 생성
 if not os.path.exists(all_dataset_train_dir):
     os.makedirs(all_dataset_train_dir)
 
-
-# if self.age_drop and (57 <= int(age) <= 59) or (28 <= int(age) <= 30): 
 
 
 # dataset/train에서 파일 복사
@@ -26,28 +35,15 @@ for folder_name in os.listdir(dataset_train_dir):
         # 중복된 폴더 처리 로직
         pass
 
-# rembg_dataset/train에서 파일 복사 및 이름 변경
+# rembg_dataset/train에서 파일 복사, 이름 중복에 의해 변경
 for folder_name in os.listdir(rembg_dataset_train_dir):
     new_folder_name = 'r' + folder_name
     source_folder = os.path.join(rembg_dataset_train_dir, folder_name)
     target_folder = os.path.join(all_dataset_train_dir, new_folder_name)
     age = int(new_folder_name.split('_')[-1])
 
-    if not os.path.exists(target_folder) and not(57 <= age <= 59 or 28 <= age <= 30):
+    if not os.path.exists(target_folder) and not(57 <= age <= 59 or 28 <= age <= 30): # age.drop 안하실거면 조건문 빼면 됩니다.
         
-        shutil.copytree(source_folder, target_folder)
-    else:
-        # 중복된 폴더 처리 로직
-        pass
-
-# yolo_dataset/train에서 파일 복사 및 이름 변경
-for folder_name in os.listdir(yolo_dataset_train_dir):
-    new_folder_name = 'y' + folder_name
-    source_folder = os.path.join(yolo_dataset_train_dir, folder_name)
-    target_folder = os.path.join(all_dataset_train_dir, new_folder_name)
-    age = int(new_folder_name.split('_')[-1])
-
-    if not os.path.exists(target_folder) and not(57 <= age <= 59 or 28 <= age <= 30):
         shutil.copytree(source_folder, target_folder)
     else:
         # 중복된 폴더 처리 로직
